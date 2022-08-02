2971d3f11037353a7093d86f0e7191fb4e416c26
Utilizador <johnny.61576.js@gmail.com>
2022-08-02 21:24:32 +0100

    School Grades

---
+++ src/main/java/school/schoolGrades/SchoolGradesApplication.java
@@ -0,0 +1,13 @@
+package school.schoolGrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolGradesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolGradesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/schoolGrades/SchoolGradesApplicationTests.java
@@ -0,0 +1,13 @@
+package school.schoolGrades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolGradesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
